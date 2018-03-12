094700517ba78d094f5524838492ef500b3bbe89
Roman Brovko <brovko-roman@ya.ru>
2018-03-12 22:13:01 +0200

    add onClickListeners for buttons

--- app/src/main/java/com/programming086/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/programming086/geoquiz/QuizActivity.kt
@@ -2,11 +2,23 @@ package com.programming086.geoquiz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_quiz.*
 
 class QuizActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
+        trueButton.setOnClickListener {
+            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+        }
+
+        falseButton.setOnClickListener {
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+        }
     }
 }
